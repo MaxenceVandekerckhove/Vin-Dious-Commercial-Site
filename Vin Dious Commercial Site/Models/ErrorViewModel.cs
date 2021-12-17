a4d44a702968e256acb7b1050c9a054bb0b07d50
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2021-12-17 11:11:05 +0100

    Ajoutez des fichiers projet.

---
+++ Vin Dious Commercial Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vin_Dious_Commercial_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
